401cc6f5dc5c64174613ddf08be7a736c7645cf6
DennerOl <dener155@hotmail.com>
2024-02-28 21:43:14 -0300

    Consulta fullSearch

--- src/main/java/com/example/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/example/workshopmongo/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.example.workshopmongo.repositories;
 
+import java.time.Instant;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -15,4 +16,11 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
   @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
   List<Post> searchTitle(String text);
+
+  // busca por posts que contenha o title informado no title ou no corpo ou no
+  // comentario
+  // com o instant de inicio e fim
+
+  @Query("{ $and: [ { moment: {$gte: ?1} }, { moment: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+  List<Post> fullSearch(String text, Instant startMoment, Instant endMoment);
 }
